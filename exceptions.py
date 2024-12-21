cd710dd65ebe3c19891079210b56d56ca8878c06
ukastsiukevich <grun_gespenst@tut.by>
2024-12-21 19:46:03 +0300

    fix: flake8 errors

--- exceptions.py
+++ exceptions.py
@@ -4,7 +4,7 @@ from fastapi import HTTPException, status
 
 
 credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Could not validate credentials",
+    headers={"WWW-Authenticate": "Bearer"},
+)
