4f128a796686d0f2dd4aee0bac1c8cd68e017e0f
ukastsiukevich <grun_gespenst@tut.by>
2024-12-21 19:42:04 +0300

    feet: add token auth

---
+++ exceptions.py
@@ -0,0 +1,10 @@
+"""Exceptions."""
+
+from fastapi import HTTPException, status
+
+
+credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 """Create and login user."""
+import datetime
 
 from fastapi import (
     Depends,
     FastAPI,
     HTTPException,
-    status,
 )
 from fastapi.security import (
     HTTPBasic,
     HTTPBasicCredentials,
+    OAuth2PasswordBearer,
 )
 from pydantic import BaseModel
-from passlib.context import CryptContext  # type: ignore
+from passlib.context import CryptContext
+from typing import Optional
+import jwt
+
+from exceptions import credentials_exception
 
 app = FastAPI()
 security = HTTPBasic()
@@ -19,8 +24,19 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 class User(BaseModel):
+    """User model."""
     username: str
     password: str
+
+
+class Token(BaseModel):
+    """Token model."""
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
 
 
 USERS = {
@@ -33,25 +49,78 @@ USERS = {
         "password": pwd_context.hash("pass2"),
     }
 }
+SECRET_KEY = "your_secret_key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = datetime.timedelta(minutes=3)
 
 
-def auth_user(credentials: HTTPBasicCredentials = Depends(security)):
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def create_access_token(data: dict, expires_delta: Optional[datetime.timedelta] = None):
+    """Creates a JWT access token."""
+    to_encode = data.copy()
+    expire = (
+        datetime.datetime.utcnow()
+        + expires_delta if expires_delta else datetime.datetime.utcnow()
+        + datetime.timedelta(minutes=15)
+    )
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def verify_password(plain_password, hashed_password):
+    """Verify the password matches the given hash."""
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def authenticate_user(username: str, password: str):
+    """Authenticate user."""
+    user = USERS.get(username)
+    if not user or not verify_password(password, user["password"]):
+        return False
+    return user
+
+
+def create_jwt_token(data: dict):
+    """Creates JWT token."""
+    data.update({"exp": datetime.datetime.utcnow() + ACCESS_TOKEN_EXPIRE_MINUTES})
+    return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+
+
+async def base_auth_user(credentials: HTTPBasicCredentials = Depends(security)):
     """Authenticate user with given credentials."""
     user = USERS.get(credentials.username)
     if user and pwd_context.verify(credentials.password, user['password']):
         return user
 
-    raise HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail='Incorrect username or password',
-        headers={'WWW-Authenticate': 'Basic'},
-    )
+
+async def token_auth_user(token: str = Depends(oauth2_scheme)):
+    """Authenticate user with given credentials."""
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        token_data = TokenData(username=username)
+        user = USERS.get(token_data.username)
+        if user:
+            return user
+    except jwt.PyJWTError:
+        raise credentials_exception
 
 
-@app.get('/login', dependencies=[Depends(auth_user)])
+@app.get('/login', dependencies=[Depends(base_auth_user) or Depends(token_auth_user)])
 async def auth():
     """Login user."""
     return 'You got my secret, welcome'
+
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(user: User):
+    """Login to user using JWT."""
+    if authenticate_user(user.username, user.password):
+        return {"access_token": create_jwt_token({"sub": user.username}), "token_type": "bearer"}
+    raise HTTPException(status_code=401, detail="Invalid credentials")
 
 
 @app.post("/create_user", response_model=User)
@@ -64,13 +133,17 @@ async def create_user(user: User):
     return user
 
 
-@app.get("/users", dependencies=[Depends(auth_user)])
+@app.get("/users", dependencies=[Depends(base_auth_user) or Depends(token_auth_user)])
 async def get_users():
     """Get all users."""
     return ', '.join([user for user in USERS])
 
 
-@app.get("/my_self", dependencies=[Depends(auth_user)], response_model=User)
+@app.get("/my_self", dependencies=[Depends(base_auth_user) or Depends(token_auth_user)], response_model=User)
 def get_my_self(credentials: HTTPBasicCredentials = Depends(security)):
     """Get my info."""
-    return auth_user(credentials)
+    if credentials:
+        return {'username': credentials.username, 'password': credentials.password}
+    elif token_auth_user:
+        return token_auth_user
+    raise credentials_exception
